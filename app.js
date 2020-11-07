1af6bf656fbb50c2df94f8eade7199341fe4abe9
tengotbp <nicoserrano555@gmail.com>
2020-11-06 22:03:34 -0300

    Creé el comando cambiarEstadoDe y agregué el comando help

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const fs = require('fs'); // File System
 let tareasJSON = fs.readFileSync('./tareas.json', 'utf8');
 let arrayDeTareas = JSON.parse(tareasJSON);
 
-switch(process.argv[2]) {
-    case 'help':
-        console.log("Comandos\n-------------\n• listarTareas\n• crearTarea 'nombre'\n• filtrarTareas 'terminada/pendiente'");
-        break;
+//---------------------------------------------------------------------------------------------------------------------------------
 
-
+switch(process.argv[2]) {  
 
 //---------------------------------------------------------------------------------------------------------------------------------
 
@@ -17,96 +14,118 @@ switch(process.argv[2]) {
     case 'listarTareas':
         console.log("Este es el listado de tareas");
         console.log("----------------------------");
-        for (let i = 0; i < arrayDeTareas.length; i++) {
-            console.log((i+1) + '. ' + arrayDeTareas[i].titulo + " • " + arrayDeTareas[i].estado);
+        if (arrayDeTareas.length < 1) {
+            console.log("    * ruido de grillos *    \n\n   Pssst... no hay tareas");
+        } else {
+            for (let i = 0; i < arrayDeTareas.length; i++) {
+                console.log((i+1) + '. ' + arrayDeTareas[i].titulo + " • " + arrayDeTareas[i].estado);
+            }
         }
         break;
+        
 
-
-
+        
 //---------------------------------------------------------------------------------------------------------------------------------
 
         // CREAR TAREA
-
+        
     case 'crearTarea':
         let nuevaTarea = {
             titulo: process.argv[3],
-            estado: 'pendiente'
+            estado: (process.argv[4] == undefined) ? "pendiente" : process.argv[4]
         }
-        arrayDeTareas.push(nuevaTarea)
-        let nuevasTareasEnJSON =  JSON.stringify(arrayDeTareas, null, 2)
-        fs.writeFileSync('./tareas.json', nuevasTareasEnJSON)
-        console.log(`Tu tarea llamada "${nuevaTarea.titulo}" fue creada con éxito!`);
+        if (process.argv[3] == undefined) {
+            console.log("Ups faltó algo... Deberías ponerle un título ¿no?");
+        } else {
+            arrayDeTareas.push(nuevaTarea) // Metí la nuevaTarea en el array arrayDeTareas
+        let nuevasTareasEnJSON =  JSON.stringify(arrayDeTareas, null, 2) // Armé una variable que guarda el arrayDeTareas en formato JSON
+        fs.writeFileSync('./tareas.json', nuevasTareasEnJSON) // Al tareas.json le agrego el array en formato JSON
+        console.log(`Tu tarea llamada "${nuevaTarea.titulo}" y su estado "${nuevaTarea.estado}" fue creada con éxito!`);
+        }
         break;
 
-
+        
 
 //---------------------------------------------------------------------------------------------------------------------------------
 
-            // BORRAR TAREA
+        // BORRAR TAREA
 
     case 'borrarTarea':
     //índice    ['node' ← 0, 'app.js' ← 1, 'borrarTarea' ← 2, 'titulo' ← 3]
     //elementos [    1     ,       2     ,          3       ,      4      ]
-    if(process.argv.length < 4){ // Pregunto si es menor que 4 elementos (por eso el .length) 
-        console.log('Te faltó poner el título de la tarea');
-    } else {
-        let tituloParaBuscar = process.argv[3].toLowerCase()
-        let tareaComparada = arrayDeTareas.filter(function(elemento){ // Itera en cada objeto literal (en este caso) del JSON → el .filter()
-            return tituloParaBuscar == elemento.titulo.toLowerCase() // Hago una comparación entre el título escrito por terminal y el título de la propiedad de cada objeto
-        })
-        if (tareaComparada.length == 1 /* ← Cantidad de elementos */) { // Acá pueden pasar dos cosas: que le haiga o que no le haiga
-            let tareasQueMeQuedan = arrayDeTareas.filter(function(elemento){ // Itera en cada objeto literal (en este caso) del JSON → el .filter()
-                return tituloParaBuscar != elemento.titulo.toLowerCase() // Que guarde todas las que NO se llamen como el título escrito por terminal
-            })
-            let tareasQueMeQuedanJSON = JSON.stringify(tareasQueMeQuedan, null, 2)
-            fs.writeFileSync("./tareas.json", tareasQueMeQuedanJSON)
-            console.log('Bien papi la borraste\n\n            Mayub85 - 2020');
-            /*console.log("Estas son todas las tareas, seleccioná alguna por su título\n----------------------------------------------------------------");
+        if(process.argv.length < 4){ // Pregunto si es menor que 4 elementos (por eso el .length)
+            console.log('Ups... Te faltó poner el título de la tarea, más específico porfis ;)');
+            console.log("\nEstas son todas las tareas, seleccioná alguna por su número\n----------------------------------------------------------------");
             for (let i = 0; i < arrayDeTareas.length; i++) {
-                console.log((i+1) + '. ' + arrayDeTareas[i].titulo + " • " + arrayDeTareas[i].estado);
-            }
-            console.log("\nCuando te asegures cuál vas a borrar, seleccionala poniendo el título(respetando los caracteres especiales) después del comando\n\nEjemplo: node app.js borrarTarea "título"");
-            */
+                    console.log((i+1) + '. ' + arrayDeTareas[i].titulo + " • " + arrayDeTareas[i].estado);
+                }
+                console.log('----------------------------------------------------------------\nCuando te asegures cuál vas a borrar, seleccionala por su número después del comando\n\n[ Ejemplo: node app.js borrarTarea 3 ]');
         } else {
-            console.log('Esta no existe padre\n\n           Mayub85 - 2020');
-        }
+            let laTareaAEliminar = process.argv[3] - 1
+            arrayDeTareas.splice(laTareaAEliminar, 1)
+            fs.writeFileSync("./tareas.json", JSON.stringify(arrayDeTareas, null, 2))
+            console.log(`¡Excelente!\n¡Tu tarea "${(arrayDeTareas[laTareaAEliminar].titulo) - 1}" ha sido borrada para siempre!`);
     }
     break;
 
 
-
+    
 //---------------------------------------------------------------------------------------------------------------------------------
 
         // FILTRAR TAREAS
-
+        
     case 'filtrarTareas':
         let estadoParaBuscar = process.argv[3]
-        let tareasFiltradas = arrayDeTareas.filter(function(elemento){
-            return estadoParaBuscar == elemento.estado
+        let tareasFiltradas = arrayDeTareas.filter(function(elemento, indice){
+            elemento.posicionOriginal = indice + 1
+            return estadoParaBuscar.toLowerCase() == (elemento.estado).toLowerCase()
         })
-        if (estadoParaBuscar == 'pendiente'){
-            console.log("Tareas pendientes\n---------------");
+            console.log(`Tareas filtradas por "${estadoParaBuscar}" son:\n-------------------------------------`);
             for (let i = 0; i < tareasFiltradas.length; i++) {
-                console.log('• ' + tareasFiltradas[i].titulo);
+                console.log(`${tareasFiltradas[i].posicionOriginal}. ${tareasFiltradas[i].titulo} • ${tareasFiltradas[i].estado}`);
             }
-        } else if (estadoParaBuscar == 'terminada'){
-            console.log("Tareas terminadas\n---------------");
-            for (let i = 0; i < tareasFiltradas.length; i++) {
-                console.log('• ' + tareasFiltradas[i].titulo);
-            }
-        } else {
-            console.log("Ups... algo falló\n\nEstados disponibles\n-------------------\n• pendiente\n• terminada");
-        }
         break;
-
 
 
 //---------------------------------------------------------------------------------------------------------------------------------
 
-            // ERROR
+        // EDITAR ESTADO DE TAREA
+
+    case 'cambiarEstadoDe':
+        let laTarea = process.argv[3]
+        let elNuevoEstado = process.argv[4]
+        if (laTarea == undefined) { // SI NO HAY TAREA
+            console.log("Ups, necesito el nombre de la tarea primero y luego el estado que quieras ponerle\n\n[ Por ejemplo: cambiarEstadoDe 'tarea' 'terminado' ]");
+            console.log(`\nEstas son todas las tareas hasta ahora\n--------------------------------------`);
+            for (let i = 0; i < arrayDeTareas.length; i++) {
+                console.log((i+1) + '. ' + arrayDeTareas[i].titulo + " • " + arrayDeTareas[i].estado);
+            }
+        } else if (elNuevoEstado == undefined) { // SI NO HAY ESTADO
+            console.log(`Tu tarea seleccionada es la número "${laTarea}", y el estado actualmente es "${arrayDeTareas[laTarea - 1].estado}", pero necesito el estado que quieras poner luego del nombre\n\nPor ejemplo: cambiarEstadoDe ${laTarea} 'terminado'`);
+        } else {
+            arrayDeTareas[laTarea - 1].estado = elNuevoEstado;
+            fs.writeFileSync('./tareas.json', JSON.stringify(arrayDeTareas, null, 2))
+        console.log(`¡Tu tarea cambió de estado a "${elNuevoEstado}" exitosamente!`);
+        console.log("--------------------------------------------");
+        console.log(`${arrayDeTareas[laTarea - 1].titulo} • ${elNuevoEstado}`);
+            }
+        break;
+
+
+//---------------------------------------------------------------------------------------------------------------------------------
+
+        // HELP
+
+    case 'help':
+        console.log("Comandos\n-------------\n• listarTareas\n• crearTarea 'nombre' 'estado' ← opcional (predeterminado: pendiente)\n• filtrarTareas 'terminada/pendiente'\n• borrarTarea 'título'");
+        break;
+
+
+//---------------------------------------------------------------------------------------------------------------------------------
+
+        // ERROR
 
     default:
-        console.log("Ups... hubo un error, para ver los comandos disponibles → help")
-        break;
-}
+        console.log("Ups... eso no lo entiendo aún, para ver los comandos disponibles escribí → help")
+            break;
+        }
